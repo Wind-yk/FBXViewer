ba3627bc7bfa808d9696ab4300931bfc9178b41c
Yikai Qiu <58819697+Wind-yk@users.noreply.github.com>
2022-07-07 11:17:31 +0200

    Update fbx.py

--- src/fbx.py
+++ src/fbx.py
@@ -3,19 +3,21 @@ import string
 import os
 
 
-def fbx2txt(input="cube.fbx", output="output.txt", force=False):
+def fbx2json(input, output="output.json", force=False):
 
     # Change working directory
     os.chdir("src/fbxInfo/")
 
     # If exit output.txt, we delete it
-    if os.path.exists(output) and force:
-        os.remove(output)
+    if os.path.exists(output):
+        if force:
+            os.remove(output)
+        else:
+            raise FileNotExist(f"{output} already exists!")
+    
 
     # Complete the command with input file name
     input = "readFbxInfo.exe " + input + " >> " + output
 
     # Execute the readFbxInfo.exe
     os.system(input)
-
-    print("Successfully converted fbx to txt!")
