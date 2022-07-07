d871270c7b7b1e85a3241014372e68d609e69d2f
Yogui <972081300@qq.com>
2022-07-07 10:41:27 +0200

    Update fbx.py

--- src/fbx.py
+++ src/fbx.py
@@ -3,17 +3,17 @@ import string
 import os
 
 
-def fbx2txt(input="cube.fbx"):
+def fbx2txt(input="cube.fbx", output="output.txt", force=False):
 
     # Change working directory
     os.chdir("src/fbxInfo/")
 
     # If exit output.txt, we delete it
-    if(os.path.exists("output.txt")):
-        os.remove("output.txt")
+    if os.path.exists(output) and force:
+        os.remove(output)
 
     # Complete the command with input file name
-    input = "readFbxInfo.exe " + input + " >> output.txt"
+    input = "readFbxInfo.exe " + input + " >> " + output
 
     # Execute the readFbxInfo.exe
     os.system(input)
