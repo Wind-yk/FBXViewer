af800d0fa20911953b56b449ebf4d3f7f794646e
Wind-yk <2389669757@qq.com>
2022-07-13 22:00:58 +0200

    move main from src/script to src/

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from packages import point
+
+print(point.Point(2,1,2))
+
+if __name__ == '__main__':
+    print('hello')
--- src/script/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append("src")
-
-import packages.fbx
-
-packages.fbx.fbx2json("sphere.fbx")
-
-print("Bye :)")
